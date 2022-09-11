70852af176950c9f9ef0ee6367a706561bbf718a
Shruti Agarwal <shrutiagr31@gmail.com>
2022-09-11 16:54:23 +0530

    first commit

--- list_basics.py
+++ list_basics.py
@@ -4,10 +4,10 @@ If two list are same but
 the order of the elements are different,
 that implies the lists are not equal
 """
-
-list_1 = [1,2,3,4,5]
-list_2 = [5,4,3,2,1]
-if(list_2==list_1):
+print("hello")
+list_1 = [1, 2, 3, 4, 5]
+list_2 = [5, 4, 3, 2, 1]
+if (list_2 == list_1):
     print(True)
 else:
     print(False)
